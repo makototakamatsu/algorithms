90c6b91354589e9a1adef8366b2de10a7f5aecf8
高松　真 <sanfranc75@gmail.com>
2017-11-13 23:05:12 +0900

    BubbleSort

--- BubbleSort.py
+++ BubbleSort.py
@@ -1,13 +1,16 @@
 
 # coding: utf-8
 
-# In[1]:
+# In[2]:
 
 #バブルソート
 def bubble_sort(array):
     n=len(array)
     for i in range(n-1):
+        #n-1からiまで-1ずつ探索
         for j in range(n-1, i, -1):
+            #j-1に格納されている数がjに格納されている数より
+            #大きいとき交換
             if array[j] < array[j-1]:
                 tmp=array[j]
                 array[j]=array[j-1]
@@ -19,7 +22,4 @@ if __name__=="__main__":
     print("before",array)
     bubble_sort(array)
     print("after",array)
-
-
-
 
